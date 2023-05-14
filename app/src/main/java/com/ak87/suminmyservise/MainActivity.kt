6a2492538262543da7e551adba4570e99ce09b72
Admin1 <a89086829981@gmail.com>
2023-05-14 19:46:19 +0300

    Create project

---
+++ app/src/main/java/com/ak87/suminmyservise/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ak87.suminmyservise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ak87.suminmyservise.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+    }
+}
